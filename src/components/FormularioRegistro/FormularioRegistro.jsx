70c83f6e6e9439201b5217fe5868f05367a2a3a5
Ricesc <ricesc14@gmail.com>
2024-06-16 22:32:02 -0400

    validation errors

--- src/components/FormularioRegistro/FormularioRegistro.jsx
+++ src/components/FormularioRegistro/FormularioRegistro.jsx
@@ -19,22 +19,48 @@ const FormularioRegistro = ({props,setProps}) => {
                     <label htmlFor="firstName">First Name</label>
                     <input type="text" name="firstName" value={props.firstName} onChange={manejarCambio} />
                 </div>
+                {
+                   props.firstName.length > 0 && props.firstName.length < 2 ?
+                    <span>First Name must be at least 2 characters</span> :
+                    ''
+                }
                 <div className="inputContainer">
                     <label htmlFor="lastName">Last Name</label>
                     <input type="text" name="lastName" value={props.lastName} onChange={manejarCambio} />
                 </div>
+                {
+                   props.lastName.length > 0 && props.lastName.length < 2 ?
+                    <span>Last Name must be at least 2 characters</span> :
+                    ''
+                }
                 <div className="inputContainer">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" value={props.email} onChange={manejarCambio} />
                 </div>
+                {
+                   props.email.length > 0 && props.email.length < 5 ?
+                    <span>Email must be at least 5 characters</span> :
+                    ''
+                }
                 <div className="inputContainer">
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password" value={props.password} onChange={manejarCambio} />
                 </div>
+                {
+                   props.password.length > 0 && props.password.length < 8 ?
+                    <span>Password must be at least 8 characters</span> :
+                    ''
+                }
                 <div className="inputContainer">
                     <label htmlFor="confirmPassword">Confirm Password</label>
                     <input type="password" name="confirmPassword" value={props.confirmPassword} onChange={manejarCambio} />
                 </div>
+                {
+                   (props.confirmPassword !== props.password && props.confirmPassword.length > 7) ||
+                   props.confirmPassword.length > props.password.length?
+                    <span>Password must match</span> :
+                    ''
+                }
             </form>        
     );
 
